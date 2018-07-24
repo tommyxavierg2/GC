758db837f4995174a50bfc7530057114c08a2314
tommyxavierg2 <tommyxavierg@gmail.com>
2018-07-24 10:06:38 -0400

    Modifications on texts, fix images not showing

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,48 +1,48 @@
 <template>
-    <div class="gc-align-center">
+    <div class="text-xs-center">
         <h1>{{title}}</h1>
         <v-container>
             <v-layout pb-4>
-                <v-container>
-                    <v-flex xs12 md8 lg8>
-                        <p class="text-sm-left"> <strong> Management of products and equipment on an international level: </strong> To be the number one intermediary agent in managing products and equipment requested by customers with manufacturers and / or distributors on an international level, offering a better option when it comes to competitive prices of the products requested.</p>
-                    </v-flex>
-                    <v-flex xs12 md4 lg4>
-                        <v-card>
-                            <v-card-media></v-card-media>
-                        </v-card>
-                    </v-flex>
-                </v-container>
+                <v-flex xs12 md8 lg8>
+                    <p class="text-sm-left"> 
+                        <strong> Management of products and equipment on an international level: </strong> To be the number one intermediary agent in managing products and equipment requested by customers with manufacturers and / or distributors on an international level, offering a better option when it comes to competitive prices of the products requested.
+                    </p>
+                </v-flex>
+                <v-flex xs12 md4 lg4>
+                    <v-card>
+                        <v-card-media :src="tradeAndServicesPics[0]" height="200px"></v-card-media>
+                    </v-card>
+                </v-flex>
             </v-layout>
+            <h4 class="text-sm-left">Trade and logistic services</h4>
             <v-layout pb-4>
-                <v-container class="text-sm-left">
-                    <h4>Trade and logistic services</h4>
-                    <v-flex xs12 md8 lg8 pt-1>
-                        <ul>
-                            <li v-for="(service, index) in tradeAndServices" :key="index" class="text-sm-left"> {{service}} </li>
-                        </ul>
-                    </v-flex>
-                    <v-flex xs12 md4 lg4 v-for="(pic, index) in tradeAndServicesPics" :key="index">
-                        <v-card>
-                            <v-card-media :src="pic"></v-card-media>
-                        </v-card>
-                    </v-flex>
-                </v-container>
+                <v-flex xs12 md8 lg8 pt-4>
+                    <ul class="text-sm-left">
+                        <li v-for="(service, index) in tradeAndServices" :key="index" class="text-sm-left"> {{service}} </li>
+                    </ul>
+                </v-flex>
+                <v-flex xs12 md4 lg4 pt-1>
+                    <v-layout row wrap v-for="(pic, index) in tradeAndServicesPics" :key="index">
+                        <v-flex xs12 md12 lg12 pt-4>
+                            <v-card>
+                                <v-card-media :src="pic" height="200px"></v-card-media>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-flex>
             </v-layout>
+            <h4 class="text-sm-left">Sevicios 3PL (third-party logistics):</h4>
             <v-layout pb-4>
-                <v-container class="text-sm-left">
-                    <h4>Sevicios 3PL (third-party logistics):</h4>
-                    <v-flex xs12 md8 lg8 pt-1>
-                        <ul>
-                            <li v-for="(service, index) in services3p" :key="index"> {{service}} </li>
-                        </ul>
-                    </v-flex>
-                    <v-flex xs12 md4 lg4>
-                        <v-card>
-                            <v-card-media :src="services3pPic"></v-card-media>
-                        </v-card>
-                    </v-flex>
-                </v-container>
+                <v-flex xs12 md8 lg8 pt-1>
+                    <ul class="text-sm-left">
+                        <li v-for="(service, index) in services3p" :key="index"> {{service}} </li>
+                    </ul>
+                </v-flex>
+                <v-flex xs12 md4 lg4 pt-1>
+                    <v-card>
+                        <v-card-media :src="services3pPic" height="200px"></v-card-media>
+                    </v-card>
+                </v-flex>
             </v-layout>
         </v-container>
     </div>
